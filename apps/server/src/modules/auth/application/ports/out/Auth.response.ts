b1be8e1f0ce34f111d2ccc9e6af0dd1dbbb0ddf7
Do Tran Thai An <justapie06@gmail.com>
2026-04-22 05:21:18 +0700

    refactor(auth): add property names to AuthResponse API documentation

--- apps/server/src/modules/auth/application/ports/out/Auth.response.ts
+++ apps/server/src/modules/auth/application/ports/out/Auth.response.ts
@@ -2,15 +2,15 @@ import { ApiProperty } from "@nestjs/swagger";
 import { Expose } from "class-transformer";
 
 export class AuthResponse {
-  @ApiProperty()
+  @ApiProperty({ name: "access_token", })
   @Expose({ name: "access_token", })
   public readonly accessToken!: string;
   
-  @ApiProperty()
+  @ApiProperty({ name: "refresh_token", })
   @Expose({ name: "refresh_token", })
   public readonly refreshToken!: string;
 
-  @ApiProperty()
+  @ApiProperty({ name: "expires_in", })
   @Expose({ name: "expires_in" })
   public readonly expiresIn!: number;
 }
