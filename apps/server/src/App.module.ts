909d176c8cc455ccd061e104d32350712578a699
Do Tran Thai An <justapie06@gmail.com>
2026-04-22 18:26:24 +0700

    feat(app): add KeyStoreModule to AppModule imports

--- apps/server/src/App.module.ts
+++ apps/server/src/App.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from "@nestjs/config";
 import { DatabaseModule } from "./modules/database/Database.module";
 import { UserModule } from "./modules/user/User.module";
 import { AuthModule } from "./modules/auth/Auth.module";
+import { KeyStoreModule } from "./modules/keystore/KeyStore.module";
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from "./modules/auth/Auth.module";
     DatabaseModule,
     UserModule,
     AuthModule,
+    KeyStoreModule,
   ]
 })
 export class AppModule {}
