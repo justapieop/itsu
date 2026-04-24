7a4d73e3712b9c4a53943268656701d1ed0b202c
Do Tran Thai An <justapie06@gmail.com>
2026-04-24 19:19:22 +0700

    refactor(decorator): simplify AuthenticatedUser decorator by removing role checks

--- apps/server/src/common/decorators/AuthenticatedUser.decorator.ts
+++ apps/server/src/common/decorators/AuthenticatedUser.decorator.ts
@@ -1,26 +1,10 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { type Request } from "express";
-import { InvalidCredentialsError } from "../../modules/auth/domain/InvalidCredentials.error";
-import { InsufficientPermissionError } from "../../modules/user/domain/InsufficientPermission.error";
-
-export interface RequiredRole {
-  requireAdmin?: boolean;
-}
 
 export const AuthenticatedUser = createParamDecorator(
-  (data: RequiredRole, ctx: ExecutionContext) => {
+  (_: any, ctx: ExecutionContext) => {
     const req: Request = ctx.switchToHttp().getRequest();
-    
-    const user = req.user;
 
-    if (!user || typeof user !== "object") {
-      throw new InvalidCredentialsError();
-    }
-
-    if (data && data.requireAdmin && !user.admin) {
-      throw new InsufficientPermissionError();
-    }
-
-    return user;
+    return req.user;
   },
 );
